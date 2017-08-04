3f3176bc93be14947c5f5f8ad2def06b111f5fc6
TMelotte <tim.melotte@hotmail.com>
2017-08-04 13:17:48 +0200

    Controllers for player and camera uploaded

---
+++ CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called every frame but guarantees that all others have run
+	void LateUpdate () {
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+		transform.position = target.transform.position + (rotation * offset);
+		transform.LookAt (target.transform);
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float rotationSpeed = 100.0f;
+
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+		}
+
+	//FixedUpdate is called when phyisics calculations will be done
+	void FixedUpdate(){
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Enemy")) {
+			Destroy (other.gameObject);
+			count++;
+		}
+	}
+}
